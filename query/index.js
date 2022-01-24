4b26638649c2084b07326b759358b64661c41534
Leo <lcheung@swellbottle.com>
2022-01-24 15:27:29 -0500

    update comment in query service after comment is updated

--- query/index.js
+++ query/index.js
@@ -17,15 +17,29 @@ app.get('/posts', (req, res) => {
 app.post('/events', (req, res) => {
   const { type, data } = req.body;
 
+  // add new post to the posts object
   if (type === 'PostCreated') {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
   }
 
+  // add new comments to the posts object
   if (type === 'CommentCreated') {
     const { id, content, postId, status } = data;
     const post = posts[postId];
     post.comments.push({ id, content, status });
+  }
+
+  // update both comment status and content in the posts object
+  if(type === 'CommentUpdated') {
+    const { id, content, postId, status } = data;
+    const post = posts[postId];
+    const comment = post.comments.find(comment => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
   }
 
   console.log(posts);
