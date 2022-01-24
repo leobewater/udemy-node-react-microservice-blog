5252d92af7093e493d5fd1648581467adc0d6977
Leo <lcheung@swellbottle.com>
2022-01-24 14:00:12 -0500

    add events route to both post and comment to receive event from the event bus

--- comments/index.js
+++ comments/index.js
@@ -40,6 +40,13 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments);
 });
 
+// receive event from event bus
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+
+  res.send({});
+});
+
 app.listen(4001, () => {
   console.log('Listening on 4001');
 });
--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {randomBytes} = require('crypto');
+const { randomBytes } = require('crypto');
 const cors = require('cors');
 const axios = require('axios');
 
@@ -11,28 +11,36 @@ app.use(cors());
 const posts = {};
 
 app.get('/posts', (req, res) => {
-    res.send(posts);
+  res.send(posts);
 });
 
 app.post('/posts', async (req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const { title } = req.body;
-    
-    posts[id] = {
-        id, title
-    };
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
 
-    // emit post created event
-    await axios.post('http://localhost:4005/events', {
-        type: 'PostCreated',
-        data: {
-            id, title
-        }
-    });
+  posts[id] = {
+    id,
+    title,
+  };
 
-    res.status(201).send(posts[id]);
+  // emit post created event
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: {
+      id,
+      title,
+    },
+  });
+
+  res.status(201).send(posts[id]);
 });
 
+// receive event from event bus
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+
+  res.send({});
+});
 
 app.listen(4000, () => {
   console.log('Listening on 4000');
