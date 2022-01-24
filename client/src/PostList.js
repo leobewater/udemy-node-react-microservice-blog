a49d0fc94f5870bcc70171d43a587b5748db85de
Leo <lcheung@swellbottle.com>
2022-01-24 12:58:32 -0500

    add () to returns

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -38,9 +38,9 @@ const PostList = () => {
     );
   });
 
-  return <div className='d-flex flex-row flex-wrap justify-content-between'>
+  return (<div className='d-flex flex-row flex-wrap justify-content-between'>
       {renderedPosts}
-  </div>;
+  </div>);
 };
 
 export default PostList;
