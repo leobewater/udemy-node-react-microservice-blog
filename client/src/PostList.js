f47e9452d9255e9d5a8f0194bf1c600bd9b81e48
Leo <lcheung@swellbottle.com>
2022-01-24 12:04:11 -0500

    add post listing component

---
+++ client/src/PostList.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const PostList = () => {
+  const [posts, setPosts] = useState({});
+
+  // get posts from api
+  const fetchPosts = async () => {
+    const res = await axios.get('http://localhost:4000/posts');
+
+    setPosts(res.data);
+  };
+
+  // run this function run time when loading component
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  // console.log(posts);
+
+  // extract all values from the posts object
+  const renderedPosts = Object.values(posts).map((post) => {
+    return (
+      <div
+        className="card"
+        style={{ width: '30%', marginBottom: '20px' }}
+        key={post.id}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
+        </div>
+      </div>
+    );
+  });
+
+  return <div className='d-flex flex-row flex-wrap justify-content-between'>
+      {renderedPosts}
+  </div>;
+};
+
+export default PostList;
